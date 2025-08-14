440f3adb55e324a2c19985a7805fe7ca9532d11f
Lipskiy Michael <miha1799@icloud.com>
2025-08-14 13:23:38 +0300

    Fix array allocation in MockRouter

---
+++ contracts/MockRouter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract MockRouter {
+    function swapExactTokensForETH(
+        uint amountIn,
+        uint /*amountOutMin*/,
+        address[] calldata /*path*/,
+        address to,
+        uint /*deadline*/
+    ) external returns (uint[] memory amounts) {
+        (bool ok, ) = payable(to).call{value: amountIn}("");
+        require(ok, "send fail");
+        amounts = new uint[](2);
+        amounts[0] = amountIn;
+        amounts[1] = amountIn;
+    }
+}
